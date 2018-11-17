7db3a7a53afb7792f2be83994add05e53f4718f4
Student <yourmail@yourmail.ru>
2018-11-17 23:00:42 +0300

    conflict resolved

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 int main(){
-	std::cout << "Hello world!\n";
+	char name[20];
+	std::cout << "Hello world!Have a nice day!\n";
+	cout<<"Enter your name:";
+	cin>>name;
+	cout<<"Hello, "<<name<<"!\n";
+	cout<<"Nice to see you learning Git\n";
 	system("pause");
 	return 0;
 }
